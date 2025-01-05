b4255ab15452cece7bbbde630843f638e73e1c92
linuxdev <asieduernest@yahoo.co.uk>
2025-01-05 02:55:38 -0500

    chore: cleanup flask

--- backend/flask/app.py
+++ backend/flask/app.py
@@ -1,37 +1,50 @@
 from flask import Flask, request, jsonify
+from flask_restx import Resource, Api, fields
+from sqlalchemy import Integer, String
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from flask_cors import CORS
-from dataclasses import dataclass
-from flask_swagger import swagger
-from flask_swagger_ui import get_swaggerui_blueprint
+from flask_alembic import Alembic
+from pprint import pprint
 import logging
 import os
-from flasgger import Swagger
+
+# from flask_swagger_ui import get_swaggerui_blueprint
+# from flasgger import Swagger
 
 app = Flask(__name__)
 CORS(app)
+api = Api(app)
 
-swagger = Swagger(app)
+
+# swagger = Swagger(app)
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 # Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///./db/todo.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, './db/todos.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
+# db = SQLAlchemy(app)
+db.init_app(app)
 
-@dataclass
-class TodoItem(db.Model):
-    __tablename__ = "todos"
-    id: int
-    title: str
-    description: str
+alembic = Alembic()
+alembic.init_app(app)
 
-    id = db.Column(db.Integer, primary_key=True, index=True)
-    title = db.Column(db.String, index=True)
-    description = db.Column(db.String, index=True)
+# # @dataclass
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String)
+    description: Mapped[str] = mapped_column(String)
 
     def to_dict(self):
         return {
@@ -40,69 +53,81 @@ class TodoItem(db.Model):
             'description': self.description
         }
 
-# @app.before_first_request
-# def create_tables():
-#     db.create_all()
+with app.app_context():
+    db.create_all()
 
+TodoModel = api.model('TodoModel', {
+        'id': fields.String,
+        'title': fields.String,
+        'description': fields.String
+})
 
-@app.route('/hello')
-def hello():
-    return 'world'
+@api.route('/todos/<int:id>','/todos/', endpoint='Todos')
+class TodoResource(Resource):
 
-@app.route('/todos/', methods=['POST'])
-def create_todo():
-    """
-    list todos
-    """
-    data = request.get_json()
-    todo = TodoItem(title=data['title'], description=data['description'])
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify(todo.to_dict())
+    @api.doc('hello')
+    # @api.route('/hello')
+    def hello(self):
+        return 'world'
 
-@app.route('/todos/', methods=['GET'])
-def read_todos():
-    skip = request.args.get('skip', 0, type=int)
-    limit = request.args.get('limit', 10, type=int)
-    todos = TodoItem.query.offset(skip).limit(limit).all()
-    return jsonify([todo.to_dict() for todo in todos])
+    @api.doc(model=[TodoModel],params={'limit':'count','skip':'skip count','id':'an id'},envelope=True)
+    def get(self,id=None,skip=0,limit=10):
+        skip = request.args.get('skip', 0, type=int)
+        limit = request.args.get('limit', 10, type=int)
+        todoQuery = db.session.query(Todo)
+        if id is None:
+            todos = todoQuery.offset(skip).limit(limit).all()
+            return jsonify([todo.to_dict() for todo in todos])
+        else:
+            todo = todoQuery.filter(Todo.id == id).first_or_404()
+            return jsonify(todo.to_dict() if todo else {})
 
-@app.route('/todos/<int:todo_id>', methods=['GET'])
-def read_todo(todo_id):
-    todo = TodoItem.query.get_or_404(todo_id)
-    return jsonify(todo.to_dict())
+    @api.doc(body=TodoModel,model=TodoModel,envelope=True)
+    def post(self):
+        data = request.get_json()
+        new_todo = Todo(
+            title=data['title'],
+            description=data['description']
+        )
+        db.session.add(new_todo)
+        db.session.commit()
+        return jsonify(new_todo.to_dict())
 
-@app.route('/todos/<int:todo_id>', methods=['PATCH'])
-def update_todo(todo_id):
-    todo = TodoItem.query.get_or_404(todo_id)
-    data = request.get_json()
-    todo.title = data['title']
-    todo.description = data['description']
-    db.session.commit()
-    return jsonify
+    @api.doc(body=TodoModel,model=TodoModel)
+    def patch(self,todo_id):
+        todo = Todo.query.get_or_404(todo_id)
+        data = request.get_json()
+        todo.title = data['title']
+        todo.description = data['description']
+        db.session.commit()
+        return jsonify(todo.to_dict())
 
-@app.route('/todos/<int:todo_id>', methods=['DELETE'])
-def delete_todo(todo_id):
-    todo = TodoItem.query.get_or_404(todo_id)
-    db.session.delete(todo)
-    db.session.commit()
-    return '', 204
+    @api.doc(params={'ids':'itemId'},model=[TodoModel])
+    def delete(self, id):
+        todo = Todo.query.get_or_404(id)
+        db.session.delete(todo)
+        db.session.commit()
+        return '', 204
 
-@app.route("/spec")
-def spec():
-    return jsonify(swagger(app))
+@api.route('/items/',endpoint='items')
+class Item(Resource):
+    @api.doc()
+    def get(self):
+        return 'hello world'
 
 # Swagger UI setup
-SWAGGER_URL = '/swagger'
-API_URL = '/static/swagger.json'
-swaggerui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "Flask API"
-    }
-)
-app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+# SWAGGER_URL = '/swagger'
+# API_URL = '/static/swagger.json'
+# swaggerui_blueprint = get_swaggerui_blueprint(
+#     SWAGGER_URL,
+#     API_URL,
+#     config={
+#         'app_name': "Flask API"
+#     }
+# )
+
+
+# app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
